bbce471361c32ad3e11a836fb4527131f8e85357
Yusuf-GitCollab <hs34kumar@gmail.com>
2021-09-29 12:12:36 +0530

    finsished

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById('my-canvas');
 
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var confettiSettings = { target: 'my-canvas' };
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
 
 console.log(canvas);
